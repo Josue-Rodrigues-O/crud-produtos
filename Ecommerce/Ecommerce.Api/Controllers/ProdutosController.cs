1f0c381552d0dc9b95ade4ff1313774faf853fb0
josue rodrigues <josuerodrigues.2003@gmail.com>
2025-08-18 11:58:05 -0300

    build: Estruturação inicial do projeto Ecommerce

---
+++ Ecommerce/Ecommerce.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        public ProdutosController() { }
+
+        [HttpGet]
+        public OkObjectResult Produtos()
+        {
+            return Ok(new object[]
+            {
+                new
+                {
+                    Id = Guid.NewGuid(),
+                    Descricao = "Teste 1",
+                    Preco = 12.50M
+                },
+                new
+                {
+                    Id = Guid.NewGuid(),
+                    Descricao = "Teste 2",
+                    Preco = 28.99M
+                },
+            });
+        }
+    }
+}
