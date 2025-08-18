cdf4a74f8142074096208128b7435b3df4431a32
josue rodrigues <josuerodrigues.2003@gmail.com>
2025-08-18 12:23:08 -0300

    feat (Ecommerce.Domain): Criação da interface IProductRepository

---
+++ Ecommerce/Ecommerce.Domain/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.Domain
+{
+    public interface IProductRepository
+    {
+        void Adicionar(Product produto);
+        IEnumerable<Product> ObterTodos();
+        Product ObterPorId(Guid id);
+        void Atualizar(Product produto);
+        void Remover(Guid id);
+    }
+}
