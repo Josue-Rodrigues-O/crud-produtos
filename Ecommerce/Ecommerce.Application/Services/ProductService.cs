1a0bea173d48a10d15bafe53436fb0a69775861e
josue rodrigues <josuerodrigues.2003@gmail.com>
2025-08-20 16:12:54 -0300

    feat (ProductService): Adicionando validações do produto relacionadas ao banco.

--- Ecommerce/Ecommerce.Application/Services/ProductService.cs
+++ Ecommerce/Ecommerce.Application/Services/ProductService.cs
@@ -1,15 +1,27 @@
 ﻿using Ecommerce.Application.Dtos;
+using Ecommerce.Domain.Exceptions;
 using Ecommerce.Domain.Filters;
 using Ecommerce.Domain.Models;
 using Ecommerce.Domain.Repositories;
 
 namespace Ecommerce.Application.Services
 {
-    public class ProductService(IProductRepository repository)
+    public class ProductService(IProductRepository repository, DepartmentService departmentService)
     {
         public Product Create(ProductDto produtoDto)
         {
+            var errors = new Dictionary<string, string[]>();
             var produto = new Product(produtoDto.Codigo, produtoDto.Descricao, produtoDto.Departamento, produtoDto.Preco);
+
+            if (GetAll(new() { Codigo = produto.Codigo, IncluirItensInativos = true }).Any())
+                errors.Add("codigo", ["Já existe um produto salvo com o código informado, por favor informe outro código."]);
+
+            if (departmentService.GetByCode(produto.Departamento) is null)
+                errors.Add("departamento", ["Não existe departamento com o código informado, por favor informe um departamento válido."]);
+
+            if (errors.Count > 0)
+                throw new ValidationException(errors);
+
             repository.Create(produto);
             return produto;
         }
@@ -26,10 +38,18 @@ namespace Ecommerce.Application.Services
 
         public void Update(Guid id, ProductDto productDto)
         {
+            var errors = new Dictionary<string, string[]>();
             var produto = GetById(id)
                 ?? throw new ArgumentException("Não foi possível encontrar o produto informado, verifique se o Id está correto.");
 
             produto.Update(productDto.Descricao, productDto.Departamento, productDto.Preco);
+
+            if (departmentService.GetByCode(produto.Departamento) is null)
+                errors.Add("departamento", ["Não existe departamento com o código informado, por favor informe um departamento válido."]);
+
+            if (errors.Count > 0)
+                throw new ValidationException(errors);
+
             repository.Update(produto);
         }
 
